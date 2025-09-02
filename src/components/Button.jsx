7d971c902c429085e43682730cb3ae6eadaddfa3
Leonardo Amarilio <134454338+LAmarilio@users.noreply.github.com>
2025-09-02 17:45:06 -0300

    Add files via upload

---
+++ src/components/Button.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Button.css'
+
+export default props =>
+    <button
+        onClick={e => props.click && props.click(props.label)}
+        className={`
+            button 
+            ${props.double ? 'double' : ''} 
+            ${props.triple ? 'triple' : ''} 
+            ${props.operation ? 'operation' : ''}
+    `}>
+        {props.label}
+    </button>
